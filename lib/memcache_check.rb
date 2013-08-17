1485aba716db008cfc57270e19baf87ff343f519
Mike Admire <mike@mikeadmire.com>
2013-08-17 08:09:31 -0500

    Refactor all memcache client and benchmark logic into Server class.

--- lib/memcache_check.rb
+++ lib/memcache_check.rb
@@ -4,75 +4,83 @@ require 'faker'
 require 'benchmark'
 
 module MemcacheCheck
-    class Checker
-        attr_reader :passes, :fails
-
-        def initialize(server = 'localhost', port = '11211')
-            @server = Server.new(server, port)
-            @passes = 0
-            @fails = 0
-        end
-
-        def start(num_times = 50)
-            time = Benchmark.measure do
-                num_times.times do
-                    test_server
-                end
-            end
-            [@passes, @fails, time.real]
-        end
-
-        def test_server
-            key, value = create_key_value_pair
-            begin
-                @server.set(key, value)
-                if is_valid?(key, value)
-                    @passes += 1
-                else
-                    @fails += 1
-                end
-            rescue
-                @fails += 1
-            end
-        end
-
-        def create_key_value_pair
-            [Utils.generate_key, Utils.generate_test_data]
-        end
-
-        def is_valid?(key, value)
-            value == @server.get(key)
-        end
+  class Checker
+    def initialize(hostname = 'localhost', port = '11211')
+      @server = Server.new(hostname, port)
     end
 
-    class Server
-        def initialize(host, port)
-            @memcache = Dalli::Client.new("#{host}:#{port}")
-        end
+    def start(num_times = 50)
+      @server.benchmark(num_times)
+      [@server.passes, @server.fails, @server.time.real]
+    end
+  end
 
-        def set(key, value)
-            @memcache.set(key, value)
-        end
+  class Server
+    attr_reader :host, :port, :passes, :fails, :time
 
-        def get(key)
-            @memcache.get(key)
-        end
+    def initialize(host = '127.0.0.1', port = '11211')
+      @memcache_client = Dalli::Client.new("#{host}:#{port}")
+      @host = host
+      @port = port
+      @passes = 0
+      @fails = 0
     end
 
-    class Utils
-        def self.generate_key
-            "MemcacheCheck" + Time.now.strftime("%s%6N")
+    def benchmark(num_times)
+      @time = Benchmark.measure do
+        num_times.times do
+          run_test
         end
-
-        def self.generate_test_data
-            {
-                name: Faker::Name.name,
-                email: Faker::Internet.email,
-                phone: Faker::PhoneNumber.phone_number,
-                city: Faker::Address.city,
-                state: Faker::Address.state,
-                bio: Faker::Lorem.words(num = 100).join(", ")
-            }
-        end
+      end
     end
+
+    def run_test
+      key, value = Utils.new.generate_key_value_pair
+      begin
+        @server.set(key, value)
+        if is_valid?(key, value)
+          @passes += 1
+        else
+          @fails += 1
+        end
+      rescue
+        @fails += 1
+      end
+    end
+
+    def set(key, value)
+      @memcache_client.set(key, value)
+    end
+
+    def get(key)
+      @memcache_client.get(key)
+    end
+
+    def is_valid?(key, value)
+      value == get(key)
+    end
+  end
+
+  class Utils
+    def generate_key_value_pair
+      [generate_key, generate_test_data]
+    end
+
+    private
+
+      def generate_key
+        "MemcacheCheck" + Time.now.strftime("%s%6N")
+      end
+
+      def generate_test_data
+        {
+          name: Faker::Name.name,
+          email: Faker::Internet.email,
+          phone: Faker::PhoneNumber.phone_number,
+          city: Faker::Address.city,
+          state: Faker::Address.state,
+          bio: Faker::Lorem.words(num = 100).join(", ")
+        }
+      end
+  end
 end
--- spec/lib/memcache_check_spec.rb
+++ spec/lib/memcache_check_spec.rb
@@ -5,34 +5,32 @@ require 'memcache_check'
 describe MemcacheCheck do
 
   describe "Checker" do
-
     before(:each) do
       @checker = MemcacheCheck::Checker.new
     end
 
     it { MemcacheCheck::Checker.should respond_to(:new) }
 
-    context "start" do
-        it { @checker.should respond_to(:start).with(1).arguments }
+    describe "start" do
+      it { @checker.should respond_to(:start).with(1).arguments }
 
-        it "returns an array" do
-            expect(@checker.start).to be_an_instance_of(Array)
-        end
+      it "returns an array" do
+        expect(@checker.start).to be_an_instance_of(Array)
+      end
 
-        it "returns an array with 3 numeric elements" do
-            passes, fails, time = @checker.start
-            expect(passes).to be_an_instance_of(Fixnum)
-            expect(fails).to be_an_instance_of(Fixnum)
-            expect(time).to be_an_instance_of(Float)
-        end
+      it "returns an array with 3 numeric elements" do
+        passes, fails, time = @checker.start
+        expect(passes).to be_an_instance_of(Fixnum)
+        expect(fails).to be_an_instance_of(Fixnum)
+        expect(time).to be_an_instance_of(Float)
+      end
 
-        it "passes and fails should add up to correct number" do
-            passes, fails, time = @checker.start(50)
-            expect(passes + fails).to eq(50)
-        end
-    end
-
-  end
+      it "passes and fails should add up to correct number" do
+        passes, fails, time = @checker.start(50)
+        expect(passes + fails).to eq(50)
+      end
+    end # context 'start'
+  end # Checker
 
   context "Server" do
     before(:each) do
@@ -49,69 +47,67 @@ describe MemcacheCheck do
     it "set and retrieve data from memcached" do
       key = "abc123"
       value = {name: 'Mike Admire',
-               text: "Lorem ipsum dolor sit amet, consectetur adipisicing" }
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing" }
       @tester.set(key, value)
       expect(@tester.get(key)).to eq(value)
     end
-  end
+  end # context 'Server'
 
 
   describe "Utils" do
-    it { MemcacheCheck::Utils.should respond_to :generate_key }
-    it { MemcacheCheck::Utils.should respond_to :generate_test_data }
+    it { MemcacheCheck::Utils.new.should respond_to :generate_key_value_pair }
 
-    context "generate_key" do
+    describe "generate_key_value_pair" do
       before(:each) do
-        @key = MemcacheCheck::Utils.generate_key
+        @key, @data = MemcacheCheck::Utils.new.generate_key_value_pair
       end
 
-      it "can generate an appropriate sized key" do
-        expect(@key.length).to be >= 25
-      end
+      context "key" do
+        it "can generate an appropriate sized key" do
+          expect(@key.length).to be >= 25
+        end
 
-      it "starts with the string MemCacheCheck in key" do
-        expect(@key).to start_with("MemcacheCheck")
-      end
+        it "starts with the string MemCacheCheck in key" do
+          expect(@key).to start_with("MemcacheCheck")
+        end
 
-      it "is unique" do
-        key1 = MemcacheCheck::Utils.generate_key
-        key2 = MemcacheCheck::Utils.generate_key
-        key3 = MemcacheCheck::Utils.generate_key
-        expect(@key).to_not eq(key1)
-        expect(@key).to_not eq(key2)
-        expect(@key).to_not eq(key3)
-        expect(key1).to_not eq(key2)
-        expect(key1).to_not eq(key3)
-        expect(key2).to_not eq(key3)
-      end
-    end # generate_key
+        it "is unique" do
+          key1 = MemcacheCheck::Utils.new.generate_key_value_pair.first
+          key2 = MemcacheCheck::Utils.new.generate_key_value_pair.first
+          key3 = MemcacheCheck::Utils.new.generate_key_value_pair.first
+          expect(@key).to_not eq(key1)
+          expect(@key).to_not eq(key2)
+          expect(@key).to_not eq(key3)
+          expect(key1).to_not eq(key2)
+          expect(key1).to_not eq(key3)
+          expect(key2).to_not eq(key3)
+        end
+      end # context 'key'
 
-    context "generate_test_data" do
-      before(:each) do
-        @data = MemcacheCheck::Utils.generate_test_data
-      end
+      context "value" do
+        it "generates a data hash" do
+          expect(@data).to be_an_instance_of(Hash)
+        end
 
-      it "generates a data hash" do
-        expect(@data).to be_an_instance_of(Hash)
-      end
+        it "is a reasonable amount of data" do
+          expect(@data.length).to be >= 5
+          expect(@data.to_s.length).to be >= 500
+        end
 
-      it "is a reasonable amount of data" do
-        expect(@data.length).to be >= 5
-        expect(@data.to_s.length).to be >= 500
-      end
+        it "is unique" do
+          data1 = MemcacheCheck::Utils.new.generate_key_value_pair.last
+          data2 = MemcacheCheck::Utils.new.generate_key_value_pair.last
+          data3 = MemcacheCheck::Utils.new.generate_key_value_pair.last
+          expect(@data).to_not eq(data1)
+          expect(@data).to_not eq(data2)
+          expect(@data).to_not eq(data3)
+          expect(data1).to_not eq(data2)
+          expect(data1).to_not eq(data3)
+          expect(data2).to_not eq(data3)
+        end
+      end # context 'value'
 
-      it "is unique" do
-        data1 = MemcacheCheck::Utils.generate_test_data
-        data2 = MemcacheCheck::Utils.generate_test_data
-        data3 = MemcacheCheck::Utils.generate_test_data
-        expect(@data).to_not eq(data1)
-        expect(@data).to_not eq(data2)
-        expect(@data).to_not eq(data3)
-        expect(data1).to_not eq(data2)
-        expect(data1).to_not eq(data3)
-        expect(data2).to_not eq(data3)
-      end
-    end # generate_test_data
+    end # generate_key_value_pair
   end # Utils
-  
+
 end
